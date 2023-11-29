a7dcaf7773a26c340d213b05daef47fde5cc60df
Hassan-Boulhilt <hboulhilt@gmail.com>
2023-11-29 01:30:39 +0100

    play audio wave and finish the app

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,3 @@
-// ignore: unused_import
 import 'package:audioplayers/audioplayers.dart';
 import 'package:flutter/material.dart';
 
@@ -6,21 +5,109 @@ void main() {
   runApp(const XylophoneApp());
 }
 
-class XylophoneApp extends StatelessWidget {
+class XylophoneApp extends StatefulWidget {
   const XylophoneApp({super.key});
 
+  @override
+  State<XylophoneApp> createState() => _XylophoneAppState();
+}
+
+class _XylophoneAppState extends State<XylophoneApp> {
+  final player = AudioPlayer();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.lightGreen.shade500,
+          backgroundColor: Colors.lightGreen.shade700,
           title: const Text(
             'Xylophone',
             style: TextStyle(color: Colors.white),
           ),
         ),
-        body: const Center(),
+        body: Center(
+          child: Column(
+            children: [
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      player.play(AssetSource('note1.wav'));
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    color: Colors.amber.shade50,
+                  )),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      player.play(AssetSource('note2.wav'));
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    color: Colors.amber[900],
+                  )),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      player.play(AssetSource('note3.wav'));
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    color: Colors.blue,
+                  )),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      player.play(AssetSource('note4.wav'));
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    color: Colors.green,
+                  )),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      player.play(AssetSource('note5.wav'));
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    color: Colors.red,
+                  )),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      player.play(AssetSource('note6.wav'));
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    color: Colors.black,
+                  )),
+              TextButton(
+                  onPressed: () {
+                    setState(() {
+                      player.play(AssetSource('note7.wav'));
+                    });
+                  },
+                  child: Container(
+                    width: double.infinity,
+                    height: 70,
+                    color: Colors.deepPurple,
+                  )),
+            ],
+          ),
+        ),
       ),
     );
   }
