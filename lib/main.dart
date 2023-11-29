848efcde83f985d53cd2e829f89f47beb36755a1
Hassan-Boulhilt <hboulhilt@gmail.com>
2023-11-29 18:38:27 +0100

    The latest version

--- lib/main.dart
+++ lib/main.dart
@@ -20,33 +20,63 @@ class _XylophoneAppState extends State<XylophoneApp> {
     });
   }
 
+  Widget returnWidget({required color, required soundNumber}) {
+    return Expanded(
+      child: TextButton(
+        style: TextButton.styleFrom(
+          backgroundColor: color,
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.zero),
+          ),
+        ),
+        onPressed: () {
+          playSound('note$soundNumber.wav');
+        },
+        child: const Text(''),
+      ),
+    );
+  }
+
+  ListView returnListViewBuilder() {
+    return ListView.builder(
+      itemExtent: 95,
+      itemCount: 7, // Number of items in the list
+      itemBuilder: (BuildContext context, int index) {
+        index = index + 1;
+        return ListTile(
+          tileColor: Colors.cyan[100 * index],
+          title: TextButton(
+            onPressed: () {
+              playSound('note$index.wav');
+            },
+            child: const Text(''),
+          ),
+        );
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            backgroundColor: Colors.lightGreen.shade500,
-            title: const Text(
-              'Xylophone',
-              style: TextStyle(color: Colors.white),
-            ),
+        appBar: AppBar(
+          backgroundColor: Colors.lightGreen.shade500,
+          title: const Text(
+            'Xylophone',
+            style: TextStyle(color: Colors.white),
           ),
-          body: ListView.builder(
-            itemExtent: 95,
-            itemCount: 7, // Number of items in the list
-            itemBuilder: (BuildContext context, int index) {
-              index = index + 1;
-              return ListTile(
-                tileColor: Colors.cyan[100 * index],
-                title: TextButton(
-                  onPressed: () {
-                    playSound('note$index.wav');
-                  },
-                  child: const Text(''),
-                ),
-              );
-            },
-          )),
+        ),
+        body: Column(crossAxisAlignment: CrossAxisAlignment.stretch, children: [
+          returnWidget(color: Colors.black, soundNumber: 1),
+          returnWidget(color: Colors.white, soundNumber: 2),
+          returnWidget(color: Colors.black, soundNumber: 3),
+          returnWidget(color: Colors.white, soundNumber: 4),
+          returnWidget(color: Colors.black, soundNumber: 5),
+          returnWidget(color: Colors.white, soundNumber: 6),
+          returnWidget(color: Colors.black, soundNumber: 7),
+        ]),
+      ),
     );
   }
 }
