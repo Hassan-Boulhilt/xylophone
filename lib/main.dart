708bc1852ebe42094cfdfe2173608f7da9fbb229
Hassan-Boulhilt <hboulhilt@gmail.com>
2023-11-29 05:33:41 +0100

    refactor code implementing a helper function to play sound and use it inside textButton widget

--- lib/main.dart
+++ lib/main.dart
@@ -14,12 +14,18 @@ class XylophoneApp extends StatefulWidget {
 
 class _XylophoneAppState extends State<XylophoneApp> {
   final player = AudioPlayer();
+  void playSound(String soundFile) {
+    setState(() {
+      player.play(AssetSource(soundFile));
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         appBar: AppBar(
-          backgroundColor: Colors.lightGreen.shade700,
+          backgroundColor: Colors.lightGreen.shade500,
           title: const Text(
             'Xylophone',
             style: TextStyle(color: Colors.white),
@@ -30,79 +36,65 @@ class _XylophoneAppState extends State<XylophoneApp> {
             children: [
               TextButton(
                   onPressed: () {
-                    setState(() {
-                      player.play(AssetSource('note1.wav'));
-                    });
+                    playSound('note1.wav');
                   },
                   child: Container(
                     width: double.infinity,
-                    height: 70,
+                    height: 50,
                     color: Colors.amber.shade50,
                   )),
               TextButton(
                   onPressed: () {
-                    setState(() {
-                      player.play(AssetSource('note2.wav'));
-                    });
+                    playSound('note2.wav');
                   },
                   child: Container(
                     width: double.infinity,
-                    height: 70,
+                    height: 50,
                     color: Colors.amber[900],
                   )),
               TextButton(
                   onPressed: () {
-                    setState(() {
-                      player.play(AssetSource('note3.wav'));
-                    });
+                    playSound('note3.wav');
                   },
                   child: Container(
                     width: double.infinity,
-                    height: 70,
+                    height: 50,
                     color: Colors.blue,
                   )),
               TextButton(
                   onPressed: () {
-                    setState(() {
-                      player.play(AssetSource('note4.wav'));
-                    });
+                    playSound('note4.wav');
                   },
                   child: Container(
                     width: double.infinity,
-                    height: 70,
+                    height: 50,
                     color: Colors.green,
                   )),
               TextButton(
                   onPressed: () {
-                    setState(() {
-                      player.play(AssetSource('note5.wav'));
-                    });
+                    playSound('note5.wav');
                   },
                   child: Container(
                     width: double.infinity,
-                    height: 70,
+                    height: 50,
                     color: Colors.red,
                   )),
               TextButton(
                   onPressed: () {
-                    setState(() {
-                      player.play(AssetSource('note6.wav'));
-                    });
+                    playSound('note6.wav');
                   },
                   child: Container(
                     width: double.infinity,
-                    height: 70,
+                    height: 50,
                     color: Colors.black,
                   )),
               TextButton(
                   onPressed: () {
-                    setState(() {
-                      player.play(AssetSource('note7.wav'));
-                    });
+                    playSound('note7.wav');
                   },
                   child: Container(
                     width: double.infinity,
-                    height: 70,
+                    height: 50,
                     color: Colors.deepPurple,
                   )),
             ],
